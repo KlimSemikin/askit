46790835cbe055220dada5216be9f2907072f18e
KlimSemikin <klim.semikin@fccho-moscow.ru>
2022-05-05 11:49:41 +0300

    style fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
+
     if @user.save
-      p session
       session[:user_id] = @user.id
       flash[:success] = "Welcome to the app, #{@user.name}!"
       redirect_to questions_path
