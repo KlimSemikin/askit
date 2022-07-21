71cebe4b27fbe61d0c9e735913fe197758e3784f
KlimSemikin <klimsemikin@gmail.com>
2022-07-21 18:36:58 +0300

    move to concern and add setting locale from headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,23 +4,5 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   include ErrorHandling
   include Authentication
-
-  around_action :switch_locale
-
-  private
-
-  def switch_locale(&action)
-    locale = locale_from_url || I18n.default_locale
-    I18n.with_locale locale, &action
-  end
-
-  def locale_from_url
-    locale = params[:locale]
-
-    locale if I18n.available_locales.map(&:to_s).include?(locale)
-  end
-
-  def default_url_options
-    { locale: I18n.locale }
-  end
+  include Internationalization
 end
