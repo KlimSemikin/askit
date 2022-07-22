f64aec88f91bd13c3176d6435a015edf34b79a14
KlimSemikin <klimsemikin@gmail.com>
2022-07-22 22:07:14 +0300

    add set_user method and refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,15 @@
 class SessionsController < ApplicationController
   before_action :require_no_authentication, only: %i[new create]
   before_action :require_authentication, only: %i[destroy]
+  before_action :set_user, only: %i[create]
 
   def new; end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user&.authenticate(params[:password])
-      do_sign_in user
+    if @user&.authenticate(params[:password])
+      do_sign_in @user
+      flash[:success] = t('.success', name: current_user.name_or_email)
+      redirect_to root_path
     else
       flash[:warning] = 'Incorrect email and/or password!'
       render :new, status: :unprocessable_entity
@@ -24,10 +26,12 @@ class SessionsController < ApplicationController
 
   private
 
+  def set_user
+    @user = User.find_by(email: params[:email])
+  end
+
   def do_sign_in(user)
     sign_in user
     remember(user) if params[:remember_me] == '1'
-    flash[:success] = "Welcome back, #{current_user.name_or_email}!"
-    redirect_to root_path
   end
 end
