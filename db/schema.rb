62bc3ec0b020bde0d98b8a7a7c2239e5500d1903
KlimSemikin <klimsemikin@gmail.com>
2022-07-25 19:52:55 +0300

    add tag uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_24_104528) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_25_141301) do
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.integer "question_id", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_24_104528) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_tags_on_title", unique: true
   end
 
   create_table "users", force: :cascade do |t|
