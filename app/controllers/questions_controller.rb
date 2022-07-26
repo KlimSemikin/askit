b00fb13aa25d6670d606bdf54cdcf99a3cfc5948
KlimSemikin <klimsemikin@gmail.com>
2022-07-26 14:50:04 +0300

    add authorization in controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,11 @@
 
 class QuestionsController < ApplicationController
   include QuestionsAnswers
+
+  before_action :require_authentication, except: %i[show index]
   before_action :set_question!, only: %i[destroy edit show update]
+  before_action :authorize_question!
+  after_action :verify_authorized
 
   def create
     @question = current_user.questions.build question_params
@@ -53,5 +57,9 @@ class QuestionsController < ApplicationController
 
   def set_question!
     @question = Question.find(params[:id])
+  end
+
+  def authorize_question!
+    authorize(@question || Question)
   end
 end
